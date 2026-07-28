87555f223807615b10930ac308666d86fc7206e1
EL HOUCINE QARA <elhoucineqara114@gmail.com>
2026-07-28 20:50:23 +0100

    Allow external images in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['mongodb'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'm.media-amazon.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images-na.ssl-images-amazon.com',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
