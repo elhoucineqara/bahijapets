84069cfb204fa03e4517f905a6a70a0997bb07b6
EL HOUCINE QARA <elhoucineqara114@gmail.com>
2026-07-28 20:12:40 +0100

    Optimize Google AdSense script location for better detection

--- src/app/layout.js
+++ src/app/layout.js
@@ -64,17 +64,16 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning className={outfit.variable}>
       <head>
         <meta name="google-adsense-account" content="ca-pub-7245366364935377" />
+        <script 
+          async 
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7245366364935377"
+          crossOrigin="anonymous"
+        ></script>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
       </head>
       <body>
         <VisitorTracker />
         {children}
-        <Script 
-          id="adsense-init"
-          strategy="afterInteractive"
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7245366364935377"
-          crossOrigin="anonymous"
-        />
       </body>
     </html>
   );
